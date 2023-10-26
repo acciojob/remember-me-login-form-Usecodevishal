dc18ccc3cfb3794e29c15c51262c95a7b4433de3
Vishal Bharti <100204338+Usecodevishal@users.noreply.github.com>
2023-10-26 19:14:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let formEle = document.querySelector("form");
+let checkEle = document.querySelector("#submit")
+formEle.addEventListener("submit", submitForm);
+
+function submitForm(e){
+	e.preventDefault();
+	console.log(checkEle.checked);
+	alert("checkbox status :", )
+}
