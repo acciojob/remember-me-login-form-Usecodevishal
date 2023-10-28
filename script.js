67424851dca25606b018e28f0d38210ce4f84616
Vishal Bharti <100204338+Usecodevishal@users.noreply.github.com>
2023-10-28 13:33:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 //your JS code here. If required.
 let formEle = document.querySelector("form");
-
+let userList = [];
 formEle.addEventListener("submit", submitForm);
 
 function submitForm(e){
 	e.preventDefault();
+	 
 	let checkEle = document.getElementById("checkbox").checked;
-	alert( `${checkEle}`);
+	let userName = document.getElementById("username").value;
+	let passWord = document.getElementById("password").value;
+	let userObj = {
+		username: userName,
+		password: passWord
+	};
+	if(checkEle){
+		userList.push(userObj);
+		localStorage.setItem("usersData", JSON.stringify(userList));
+	}
+	alert( `Logged in as ${userName}`);
 }
